15a8544d73235d5f97695fa19aff5230cbed09e4
eneperezv <eneperezv@gmail.com>
2024-10-13 23:00:37 -0500

    Implementacion clase Notifications
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/model/Notification.java

--- blabber.api/src/main/java/com/enp/blabber/api/model/Notification.java
+++ blabber.api/src/main/java/com/enp/blabber/api/model/Notification.java
@@ -1,5 +1,93 @@
 package com.enp.blabber.api.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "dbo_notifications")
 public class Notification {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="id_notification", unique=true, nullable=false)
+	private Long id;
+	
+	@Column(name = "id_user", nullable = false)
+	private User user;
+	
+	@Column(name = "message", nullable = false)
+	private String message;
+	
+	@Column(name = "is_read", nullable = false)
+	private boolean isRead;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	public Notification() {
+		
+	}
+
+	public Notification(User user, String message, boolean isRead, LocalDateTime createdAt) {
+		super();
+		this.user = user;
+		this.message = message;
+		this.isRead = isRead;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isRead() {
+		return isRead;
+	}
+
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Notification [id=" + id + ", user=" + user + ", message=" + message + ", isRead=" + isRead
+				+ ", createdAt=" + createdAt + "]";
+	}
 
 }
