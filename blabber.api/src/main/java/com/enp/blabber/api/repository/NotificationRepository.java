daaf159dc3d972213fd16021c7e59850fb67e315
eneperezv <eneperezv@gmail.com>
2024-10-15 23:46:33 -0500

    Implementacion inicial de clases service y repository para directmessages y notifications
     Changes to be committed:
    	new file:   blabber.api/src/main/java/com/enp/blabber/api/repository/DirectMessageRepository.java
    	new file:   blabber.api/src/main/java/com/enp/blabber/api/repository/NotificationRepository.java
    	new file:   blabber.api/src/main/java/com/enp/blabber/api/service/DirectMessageService.java
    	new file:   blabber.api/src/main/java/com/enp/blabber/api/service/NotificationService.java

---
+++ blabber.api/src/main/java/com/enp/blabber/api/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.enp.blabber.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enp.blabber.api.model.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+
+}
