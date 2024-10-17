e890959403f701d5dd3f175ea2dee9c833740435
eneperezv <eneperezv@gmail.com>
2024-10-16 23:22:06 -0500

    Se inicia implementacion de clase NotificationController
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/controller/NotificationController.java

--- blabber.api/src/main/java/com/enp/blabber/api/controller/NotificationController.java
+++ blabber.api/src/main/java/com/enp/blabber/api/controller/NotificationController.java
@@ -1,10 +1,16 @@
 package com.enp.blabber.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.enp.blabber.api.service.NotificationService;
 
 @RestController
 @RequestMapping("/api/v1/blabber/notifications")
 public class NotificationController {
+	
+	@Autowired
+	private NotificationService notificationService;
 
 }
