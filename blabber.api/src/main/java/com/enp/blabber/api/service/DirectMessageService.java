6c9e4210ccf8599495476e2ca4fa6de060d7c5b3
eneperezv <eneperezv@gmail.com>
2024-10-16 23:24:23 -0500

    Se inicia implementacion de DirectMessageService
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/DirectMessageService.java

--- blabber.api/src/main/java/com/enp/blabber/api/service/DirectMessageService.java
+++ blabber.api/src/main/java/com/enp/blabber/api/service/DirectMessageService.java
@@ -1,8 +1,14 @@
 package com.enp.blabber.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.enp.blabber.api.repository.DirectMessageRepository;
 
 @Service
 public class DirectMessageService {
+	
+	@Autowired
+	private DirectMessageRepository dmRepository;
 
 }
