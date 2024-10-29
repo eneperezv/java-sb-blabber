44f4b48c55ce1b3af059175b7e65f1fc066c7da6
eneperezv <eneperezv@gmail.com>
2024-10-29 07:53:17 -0500

    Se reinicia el desarrollo de la api. Se eliminan todos los paquetes y clases para iniciar nuevamente el desarrollo.
     Changes to be committed:
    	deleted:    blabber.api/logs/blabber.api.log.2024-10-08.0.gz
    	deleted:    blabber.api/logs/blabber.api.log.2024-10-13.0.gz
    	deleted:    blabber.api/logs/blabber.api.log.2024-10-14.0.gz
    	new file:   blabber.api/logs/blabber.api.log.2024-10-15.0.gz
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/AuthController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/BlabController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/DirectMessageController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/FollowController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/NotificationController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/controller/UserController.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/CommentDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/DirectMessageDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/FollowDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/LikeDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/NotificationDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/dto/UserDto.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Blab.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Comment.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/DirectMessage.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/ErrorDetails.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Follow.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Like.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Notification.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/ResponseDetails.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/Token.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/User.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/model/UserMyDetails.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/repository/BlabRepository.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/repository/DirectMessageRepository.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/repository/FollowRepository.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/repository/NotificationRepository.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/repository/UserRepository.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/security/JwtAuthenticationFilter.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/security/JwtService.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/security/LoginForm.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/security/WebSecurityConfig.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/service/BlabService.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/service/DirectMessageService.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/service/FollowService.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/service/NotificationService.java
    	deleted:    blabber.api/src/main/java/com/enp/blabber/api/service/UserService.java

--- blabber.api/src/main/java/com/enp/blabber/api/repository/DirectMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enp.blabber.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.enp.blabber.api.model.DirectMessage;
-
-public interface DirectMessageRepository extends JpaRepository<DirectMessage,Long> {
-
-}
--- blabber.api/src/main/java/com/enp/blabber/api/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enp.blabber.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.enp.blabber.api.model.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification,Long> {
-
-}
