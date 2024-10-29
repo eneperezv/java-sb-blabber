0fe0abecae14098f7650f988a3f62f0ad84e8f58
eneperezv <eneperezv@gmail.com>
2024-10-29 08:50:37 -0500

    Implementacion de clases para gestion de Blab
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/model/Blab.java

--- blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
+++ blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
@@ -1,5 +1,50 @@
 package com.enp.blabber.api.dto;
 
+import java.time.LocalDateTime;
+
 public class BlabDto {
+	
+	private Long id;
+    private String content;
+    private String username;
+    private LocalDateTime createdAt;
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "BlabDto [id=" + id + ", content=" + content + ", username=" + username + ", createdAt=" + createdAt
+				+ "]";
+	}
 
 }
--- blabber.api/src/main/java/com/enp/blabber/api/model/Blab.java
+++ blabber.api/src/main/java/com/enp/blabber/api/model/Blab.java
@@ -1,5 +1,79 @@
 package com.enp.blabber.api.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="dbo_blabs")
 public class Blab {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="id_blab", unique=true, nullable=false)
+	private Long id;
+	
+	@Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    private LocalDateTime createdAt;
+
+	public Blab(String content, User user, LocalDateTime createdAt) {
+		super();
+		this.content = content;
+		this.user = user;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Blab [id=" + id + ", content=" + content + ", user=" + user + ", createdAt=" + createdAt + "]";
+	}
 
 }
