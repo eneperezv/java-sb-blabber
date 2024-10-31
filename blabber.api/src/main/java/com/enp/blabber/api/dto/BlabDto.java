afcd399526de94bf2820255425b5b382a49c2800
eneperezv <eneperezv@gmail.com>
2024-10-30 23:18:21 -0500

    Inclusion de documentacion inicial de clases
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/dto/UserDto.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/BlabService.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/UserService.java

--- blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
+++ blabber.api/src/main/java/com/enp/blabber/api/dto/BlabDto.java
@@ -1,8 +1,21 @@
 package com.enp.blabber.api.dto;
 
-import java.time.LocalDateTime;
+/*
+ * @(#)BlabDto.java 1.0 29/10/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
-import com.enp.blabber.api.model.User;
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
+import java.time.LocalDateTime;
 
 public class BlabDto {
 	
