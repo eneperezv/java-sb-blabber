7b02af1547b7bef62116db7cdb12c009dc1f653a
eneperezv <eneperezv@gmail.com>
2024-10-31 16:16:28 -0500

    Se incluye documentacion inicial de las clases
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/repository/LikeRepository.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/CommentService.java
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/LikeService.java

--- blabber.api/src/main/java/com/enp/blabber/api/service/LikeService.java
+++ blabber.api/src/main/java/com/enp/blabber/api/service/LikeService.java
@@ -1,11 +1,25 @@
 package com.enp.blabber.api.service;
 
+/*
+ * @(#)LikeService.java 1.0 31/10/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.enp.blabber.api.dto.BlabDto;
 import com.enp.blabber.api.dto.LikeDto;
 import com.enp.blabber.api.model.Like;
 import com.enp.blabber.api.repository.LikeRepository;
