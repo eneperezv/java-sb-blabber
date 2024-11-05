8bb87b375a1eb4e7961428e54253a85483532ee3
eneperezv <eneperezv@gmail.com>
2024-11-04 23:19:04 -0500

    Inicio de implementacion de NotificationService
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/service/NotificationService.java

--- blabber.api/src/main/java/com/enp/blabber/api/service/NotificationService.java
+++ blabber.api/src/main/java/com/enp/blabber/api/service/NotificationService.java
@@ -1,8 +1,19 @@
 package com.enp.blabber.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.enp.blabber.api.dto.NotificationDto;
+import com.enp.blabber.api.repository.NotificationRepository;
 
 @Service
 public class NotificationService {
+	
+	@Autowired
+	private NotificationRepository notificationRepository;
+	
+	public void createNotification(NotificationDto notificationDto) {
+		
+	}
 
 }
