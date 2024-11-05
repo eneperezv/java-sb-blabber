1ee6cfbd32b3b086d7a661aed78fc41182ab685d
eneperezv <eneperezv@gmail.com>
2024-11-05 00:05:15 -0500

    Se organiza clase dataUtils para que traiga la hora de colombia
     Changes to be committed:
    	modified:   blabber.api/src/main/java/com/enp/blabber/api/utils/DataUtils.java

--- blabber.api/src/main/java/com/enp/blabber/api/utils/DataUtils.java
+++ blabber.api/src/main/java/com/enp/blabber/api/utils/DataUtils.java
@@ -1,22 +1,7 @@
 package com.enp.blabber.api.utils;
 
-/*
- * @(#)DataUtils.java 1.0 29/10/2024
- * 
- * El código implementado en este formulario esta protegido
- * bajo las leyes internacionales del Derecho de Autor, sin embargo
- * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
- * descrita en https://www.gnu.org/licenses/gpl-3.0.html
- */
-
-/**
- * @author eliezer.navarro
- * @version 1.0
- * @since 1.0
- */
-
-import java.time.Clock;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.springframework.stereotype.Service;
 
@@ -24,8 +9,8 @@ import org.springframework.stereotype.Service;
 public class DataUtils {
 	
 	public LocalDateTime getTimeNow() {
-		Clock cl = Clock.systemUTC(); 
-		return LocalDateTime.now(cl);
+		ZoneId colombiaZone = ZoneId.of("America/Bogota");
+		return LocalDateTime.now(colombiaZone);
 	}
 
 }
